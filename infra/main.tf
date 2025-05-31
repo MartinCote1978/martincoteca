de507fe7a97012c9790a4dfbf151f0c30e8514cd
Martin Cote <martin.cote@gmail.com>
2025-05-31 11:23:04 -0400

    Trying without CDN to save on costs.

--- infra/main.tf
+++ infra/main.tf
@@ -56,7 +56,8 @@ resource "google_compute_backend_bucket" "martincoteca_website_backend" {
   name        = "martincoteca-website-backend"
   description = "Contains files needed by the website"
   bucket_name = google_storage_bucket.martincoteca_website.name
-  enable_cdn  = true
+  #enable_cdn  = true
+  enable_cdn = false
   # cdn_policy {
   #   cache_mode = "CACHE_ALL_STATIC"
   #   client_ttl = 3600  # 1 hour
